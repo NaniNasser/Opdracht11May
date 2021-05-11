e69632b2b2a1342a7d53d607228dafb55b7f67c3
Nani <83227319+NaniNasser@users.noreply.github.com>
2021-05-11 19:14:50 +0200

    Add files via upload

---
+++ Bord.java
@@ -0,0 +1,8 @@
+package Opdrachten;
+
+public abstract class Bord implements Oppervlakte {
+    private String materiaal;
+    private String kleur;
+
+}
+
---
+++ Oppervlakte.java
@@ -0,0 +1,5 @@
+package Opdrachten;
+
+public interface Oppervlakte {
+     double  oppervlakte();
+}
